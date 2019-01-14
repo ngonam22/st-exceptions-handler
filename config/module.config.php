e05c1c7588921a1426b18e9f25c7417f1291b3b5
J Ngo <ngonam22@gmail.com>
2019-01-14 13:18:17 +0700

    release the test version

---
+++ config/module.config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: namngo
+ * Date: 1/14/19
+ * Time: 1:15 PM
+ */
+
+namespace StExceptionsHandler;
+
+return [
+    'stexceptionshandler' => [
+        'helloo' => true
+    ]
+];
---
+++ src/Module.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: namngo
+ * Date: 1/14/19
+ * Time: 1:14 PM
+ */
+
+namespace StExceptionsHandler;
+
+use Zend\EventManager\EventInterface;
+
+use Zend\ModuleManager\Feature\BootstrapListenerInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements ConfigProviderInterface, BootstrapListenerInterface
+{
+    /**
+     * Return default zend-serializer configuration for zend-mvc applications.
+     */
+    public function getConfig() {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+
+    /**
+     * Listen to the bootstrap event
+     *
+     * @param \Zend\Mvc\MvcEvent|EventInterface $e
+     * @return void
+     */
+    public function onBootstrap(EventInterface $e) {
+
+        dd('im here');
+    }
+
+}
